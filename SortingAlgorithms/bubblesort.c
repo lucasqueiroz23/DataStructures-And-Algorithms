abbff46e111176dd6988b6900817ed94b5bcad1c
Lucas Queiroz <86672557+lucasqueiroz23@users.noreply.github.com>
2022-05-02 16:16:16 -0300

    added bubblesort

---
+++ SortingAlgorithms/bubblesort.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+// This implementation of bubblesort is for integer arrays. But since the algorithm can be used to sort any kind of data type,
+// I used typedef to illustrate that it can be adapted to any other kind of data type.
+typedef int DATA_TYPE;
+
+void bubbleSort(DATA_TYPE *array, int arraySize)
+{
+    DATA_TYPE temporaryValue = 0;
+    for(int i = 0; i < arraySize - 1 ; i++)
+    {
+        for(int j = 0; j < arraySize - 1; j++)
+        {
+            if(v[j+1] < v[j])
+            {
+                temporaryValue = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temporaryValue;
+            }
+        }
+    }
+}
+
