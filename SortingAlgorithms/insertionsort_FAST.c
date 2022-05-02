e08e128fb7639b5f319731348db49a940b619169
Lucas Queiroz <86672557+lucasqueiroz23@users.noreply.github.com>
2022-05-02 15:13:39 -0300

    Update insertionsort_FAST.c

--- SortingAlgorithms/insertionsort_FAST.c
+++ SortingAlgorithms/insertionsort_FAST.c
@@ -6,23 +6,23 @@ typedef int DATA_TYPE;
 
 void insertionSort(DATA_TYPE *v, int leftIndex, int rightIndex)
 {
-    int temporaryValue=0;
-    for(int i=leftIndex; i<rightIndex; i++)
+    DATA_TYPE temporaryValue = 0;
+    for(int i = leftIndex; i < rightIndex; i++)
     {
 
-        if(v[i]>v[i+1])
+        if(v[i] > v[i+1])
         {
-            temporaryValue=v[i];
-            v[i]=v[i+1];
-            v[i+1]=temporaryValue;
+            temporaryValue = v[i];
+            v[i] = v[i+1];
+            v[i+1] = temporaryValue;
 
         }
     }
-    for(int i=leftIndex; i<rightIndex; i++)
+    for(int i = leftIndex; i < rightIndex; i++)
     {
-        for(int j = i+1; j>leftIndex; j--)
+        for(int j = i+1; j > leftIndex; j--)
         {
-            if(v[j]<v[j-1])
+            if(v[j] < v[j-1])
             {
                 temporaryValue = v[j-1];
                 v[j-1] = v[j];
