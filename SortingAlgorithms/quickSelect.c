a8ab8c8a46226a7e75cb057a37209e496f34cdc0
Lucas <lucas.fn4@gmail.com>
2023-06-13 11:44:19 -0300

    Correction and refactor of quickselect

--- SortingAlgorithms/quickSelect.c
+++ SortingAlgorithms/quickSelect.c
@@ -4,15 +4,13 @@
 // I used typedef to illustrate that it can be adapted to any other kind of data type.
 typedef int DATA_TYPE;
 
-int partition(DATA_TYPE *v,int leftIndex, int rightIndex)
-{
+int partition(DATA_TYPE* v, int leftIndex, int rightIndex) {
 
     DATA_TYPE valueToCompare = v[rightIndex];
     int j = leftIndex;
 
-    for(int k = leftIndex; k< rightIndex ; k++)
-        if(v[k]<=valueToCompare)
-        {
+    for(int k = leftIndex; k < rightIndex; k++)
+        if(v[k]<=valueToCompare) {
 
             int temporaryValue = v[k];
             v[k] = v[j];
@@ -22,22 +20,16 @@ int partition(DATA_TYPE *v,int leftIndex, int rightIndex)
         }
 
     int temporaryValue = v[j];
-    v[j] = v[r];
-    v[r] = temporaryValue;
-
+    v[j] = v[rightIndex];
+    v[rightIndex] = temporaryValue;
 
     return j;
 
-
 }
 
-void quickSelect(DATA_TYPE *v, DATA_TYPE valueToSort, int leftIndex, int rightIndex)
-{
-	DATA_TYPE j = partition(v,leftIndex,rightIndex);
+void quickSelect(DATA_TYPE* v, DATA_TYPE valueToSort, int leftIndex, int rightIndex) {
+	DATA_TYPE j = partition(v, leftIndex, rightIndex);
 	if(j == valueToSort) return;
-	if(j<valueToSort)
-		quickSelect(v,valueToSort,j+1,rightIndex);
-	else
-		quickSelect(v,valueToSort,leftIndex,j-1);
-
+	if(j < valueToSort) quickSelect(v, valueToSort, j+1, rightIndex);
+	else quickSelect(v, valueToSort, leftIndex, j-1);
 }
