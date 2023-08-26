b239c8d35cddb1b9218afa0dbff1e6ae97ca3813
Lucas <lucas.fn4@gmail.com>
2023-08-25 22:21:37 -0300

    Adds headers for slow version of insertionsort

--- SortingAlgorithms/insertionsort_SLOW.c
+++ SortingAlgorithms/insertionsort_SLOW.c
@@ -1,21 +1,15 @@
-#include <stdlib.h>
+#include "swap.h"
+#include "insertionsort_SLOW.h"
 
-// This implementation of insertionsort is for integer arrays. But since the algorithm can be used to sort any kind of data type,
-// I used typedef to illustrate that it can be adapted to any other kind of data type.
-typedef int DATA_TYPE;
-
-void insertionSort(DATA_TYPE *v, int ARRAY_SIZE)
+void insertionSort(int *array, const int ARRAY_SIZE)
 {
-    DATA_TYPE temporaryValue=0;
-    for(int i=0; i<ARRAY_SIZE; i++)
+    for(int i = 0; i < ARRAY_SIZE; i++)
     {
-        for(int j = i+1; j>0; j--)
+        for(int j = i + 1; j > 0; j--)
         {
-            if(v[j]<v[j-1])
+            if(array[j] < array[j-1])
             {
-                temporaryValue = v[j-1];
-                v[j-1] = v[j];
-                v[j] = temporaryValue;
+                swap(array, j, j - 1);
             }
             else break;
         }
---
+++ SortingAlgorithms/insertionsort_SLOW.h
@@ -0,0 +1,8 @@
+#ifndef __INSERTIONSORT_SLOW_H__
+#define __INSERTIONSORT_SLOW_H__
+
+#include "swap.h"
+
+void insertionSort(int *array, const int ARRAY_SIZE);
+
+#endif
