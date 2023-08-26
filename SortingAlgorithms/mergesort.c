6444e5d4450d9f0f3c1c93ce7eb6e0eedca2e60c
Lucas <lucas.fn4@gmail.com>
2023-08-25 22:11:39 -0300

    Adds header files for mergesort

--- SortingAlgorithms/mergesort.c
+++ SortingAlgorithms/mergesort.c
@@ -1,51 +1,45 @@
 #include <stdlib.h>
+#include "mergesort.h"
 
-// This implementation of mergesort is for integer arrays. But since the algorithm can be used to sort any kind of data type,
-// I used typedef to illustrate that it can be adapted to any other kind of data type.
-typedef int DATA_TYPE;
-
-void merge(DATA_TYPE *v, int leftIndex, int middleIndex, int rightIndex)
+void merge(int *array, int leftIndex, int middleIndex, int rightIndex)
 {
-    DATA_TYPE *v2 = malloc(sizeof(DATA_TYPE)*(rightIndex-leftIndex+1));
+    int *v2 = malloc(sizeof(int) * (rightIndex - leftIndex + 1));
 
-    int k = 0;  // k is an index that will be used to traverse through v2
+    int k = 0;  
     int i = leftIndex;
-    int j = middleIndex+1;
+    int j = middleIndex + 1;
 
-    while(i<=middleIndex && j<=rightIndex)
+    while(i <= middleIndex && j <= rightIndex)
     {
-        if(v[i]<=v[j])
+        if(array[i]<=array[j])
         {
-            v2[k++] = v[i++];
+            v2[k++] = array[i++];
         }
-        else v2[k++] = v[j++];
+        else v2[k++] = array[j++];
     }
 
-    while(i<=middleIndex)
-        v2[k++] = v[i++];
-    while(j<=rightIndex)
-        v2[k++] = v[j++];
+    while(i <= middleIndex)
+        v2[k++] = array[i++];
+    while(j <= rightIndex)
+        v2[k++] = array[j++];
 
-    k=0;
+    k = 0;
 
-    for(i = leftIndex; i<=rightIndex; i++)
+    for(i = leftIndex; i <= rightIndex; i++)
     {
-        v[i] = v2[k++];
+        array[i] = v2[k++];
     }
-
 
     free(v2);
-
 }
 
-void mergeSort(DATA_TYPE *v, int leftIndex, int rightIndex)
+void mergeSort(int *array, int leftIndex, int rightIndex)
 {
-    if(leftIndex>=rightIndex) return;
-    int middleIndex = (leftIndex+rightIndex)/2;
-    mergeSort(v,leftIndex,middleIndex);
-    mergeSort(v,middleIndex+1,rightIndex);
-    merge(v,leftIndex,middleIndex,rightIndex);
-
+    if(leftIndex >= rightIndex) return;
+    int middleIndex = (leftIndex + rightIndex)/2;
+    mergeSort(array, leftIndex, middleIndex);
+    mergeSort(array, middleIndex + 1, rightIndex);
+    merge(array, leftIndex, middleIndex, rightIndex);
 }
 
 
---
+++ SortingAlgorithms/mergesort.h
@@ -0,0 +1,9 @@
+#ifndef __MERGESORT_H__
+#define __MERGESORT_H__
+
+
+void merge(int *array, int leftIndex, int middleIndex, int rightIndex);
+
+void mergeSort(int *array, int leftIndex, int rightIndex);
+
+#endif
