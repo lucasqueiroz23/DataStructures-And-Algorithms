e2f08295483b51accd644ad7201ce62bc3915dde
Lucas <lucas.fn4@gmail.com>
2023-08-25 22:48:00 -0300

    Adds header for quicksort

--- SortingAlgorithms/quicksortM3insertion.c
+++ SortingAlgorithms/quicksortM3insertion.c
@@ -1,106 +1,32 @@
-#include <stdlib.h>
+#include "swap.h"
+#include "insertionsort_FAST.h"
+#include "partition.h"
 
-// This implementation of quicksort is for integer arrays. But since the algorithm can be used to sort any kind of data type,
-// I used typedef to illustrate that it can be adapted to any other kind of data type.
-typedef int DATA_TYPE;
 
-void insertionSort(DATA_TYPE *v, int leftIndex, int rightIndex)
-{
-    int temporaryValue=0;
-    for(int i=leftIndex; i<rightIndex; i++)
-    {
-
-        if(v[i]>v[i+1])
-        {
-            temporaryValue=v[i];
-            v[i]=v[i+1];
-            v[i+1]=temporaryValue;
-
-        }
-    }
-    for(int i=leftIndex; i<rightIndex; i++)
-    {
-        for(int j = i+1; j>leftIndex; j--)
-        {
-            if(v[j]<v[j-1])
-            {
-                temporaryValue = v[j-1];
-                v[j-1] = v[j];
-                v[j] = temporaryValue;
-            }
-            else break;
-        }
-    }
-
-}
-
-int partition(DATA_TYPE *v,int leftIndex, int rightIndex)
+void quickSort(int* array, int leftIndex, int rightIndex)
 {
 
-    DATA_TYPE valueToCompare = v[rightIndex];
-    int j = leftIndex;
-
-    for(int k = leftIndex; k< rightIndex ; k++)
-        if(v[k]<=valueToCompare)
-        {
-
-            int temporaryValue = v[k];
-            v[k] = v[j];
-            v[j] = temporaryValue;
-            j++;
-
-        }
-
-    int temporaryValue = v[j];
-    v[j] = v[rightIndex];
-    v[rightIndex] = temporaryValue;
-
-
-    return j;
-
-
-}
-
-void quickSort(DATA_TYPE *v, int leftIndex, int rightIndex)
-{
-
-    if(rightIndex-leftIndex<=32)
+    if(rightIndex-leftIndex <= 32)
     {
-        insertionSort(v,leftIndex,rightIndex);
+        insertionSort(array, leftIndex, rightIndex);
         return;
 
     }
-    int middleIndex = (leftIndex+rightIndex)/2;
+    int middleIndex = (leftIndex + rightIndex)/2;
 
-    DATA_TYPE middleValue = v[middleIndex];
-    v[middleIndex] = v[rightIndex-1];
-    v[rightIndex-1] = middleValue;
+    int middleValue = array[middleIndex];
+    swap(array, middleIndex, rightIndex - 1);
 
-    if(v[leftIndex]>v[rightIndex-1])
-    {
-        DATA_TYPE temporaryValue = v[leftIndex];
-        v[leftIndex] = v[rightIndex-1];
-        v[rightIndex-1] = temporaryValue;
-    }
+    if(array[leftIndex] > array[rightIndex - 1])
+        swap(array, leftIndex, rightIndex - 1);
 
-    if(v[rightIndex]<v[leftIndex])
-    {
-        DATA_TYPE temporaryValue = v[rightIndex];
-        v[rightIndex] = v[leftIndex];
-        v[leftIndex] = temporaryValue;
-    }
+    if(array[rightIndex] < array[leftIndex])
+        swap(array, leftIndex, rightIndex);
 
-    if(v[middleIndex]<v[rightIndex-1])
-    {
-        DATA_TYPE temporaryValue = v[rightIndex];
-        v[rightIndex] = v[rightIndex-1];
-        v[rightIndex-1] = temporaryValue;
-    }
+    if(array[middleIndex] < array[rightIndex - 1])
+        swap(array, middleIndex, rightIndex - 1);
 
-
-    int j = partition(v,leftIndex,rightIndex);
-    quickSort(v,leftIndex,j-1);
-    quickSort(v,j+1,rightIndex);
-
-
+    int j = partition(array, leftIndex,rightIndex);
+    quickSort(array, leftIndex, j - 1);
+    quickSort(array, j + 1, rightIndex);
 }
---
+++ SortingAlgorithms/quicksortM3insertion.h
@@ -0,0 +1,11 @@
+#ifndef __QUICKSORTM3INSERTION_H__
+#define __QUICKSORTM3INSERTION_H__
+
+#include "swap.h"
+#include "insertionsort_FAST.h"
+#include "partition.h"
+
+
+void quickSort(int* array, int leftIndex, int rightIndex);
+
+#endif
