7cff378380de552f80a62e8ce475dbee5f9328f6
Lucas <lucas.fn4@gmail.com>
2023-08-25 22:29:00 -0300

    Adds header for quickSelect

--- SortingAlgorithms/quickSelect.c
+++ SortingAlgorithms/quickSelect.c
@@ -1,35 +1,10 @@
 #include <stdlib.h>
+#include "partition.h"
 
-// This implementation of quickSelect is for integer arrays. But since the algorithm can be used to sort any kind of data type,
-// I used typedef to illustrate that it can be adapted to any other kind of data type.
-typedef int DATA_TYPE;
 
-int partition(DATA_TYPE* v, int leftIndex, int rightIndex) {
-
-    DATA_TYPE valueToCompare = v[rightIndex];
-    int j = leftIndex;
-
-    for(int k = leftIndex; k < rightIndex; k++)
-        if(v[k]<=valueToCompare) {
-
-            int temporaryValue = v[k];
-            v[k] = v[j];
-            v[j] = temporaryValue;
-            j++;
-
-        }
-
-    int temporaryValue = v[j];
-    v[j] = v[rightIndex];
-    v[rightIndex] = temporaryValue;
-
-    return j;
-
-}
-
-void quickSelect(DATA_TYPE* v, DATA_TYPE valueToSort, int leftIndex, int rightIndex) {
-	DATA_TYPE j = partition(v, leftIndex, rightIndex);
+void quickSelect(int* array, int valueToSort, int leftIndex, int rightIndex) {
+	int j = partition(array, leftIndex, rightIndex);
 	if(j == valueToSort) return;
-	if(j < valueToSort) quickSelect(v, valueToSort, j+1, rightIndex);
-	else quickSelect(v, valueToSort, leftIndex, j-1);
+	if(j < valueToSort) quickSelect(array, valueToSort, j+1, rightIndex);
+	else quickSelect(array, valueToSort, leftIndex, j-1);
 }
---
+++ SortingAlgorithms/quickSelect.h
@@ -0,0 +1,8 @@
+#ifndef __QUICKSELECT_H__
+#define __QUICKSELECT_H__
+
+#include "partition.h"
+
+void quickSelect(int* array, int valueToSort, int leftIndex, int rightIndex);
+
+#endif
