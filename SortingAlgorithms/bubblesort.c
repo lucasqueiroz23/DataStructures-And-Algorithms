bb2cce0882c2287fc58716b3889e982ca62b6fb9
Lucas <lucas.fn4@gmail.com>
2023-08-25 22:18:06 -0300

    Adds header files for bubble sort

--- SortingAlgorithms/bubblesort.c
+++ SortingAlgorithms/bubblesort.c
@@ -1,21 +1,15 @@
-#include <stdlib.h>
+#include "swap.h"
+#include "bubblesort.h"
 
-// This implementation of bubblesort is for integer arrays. But since the algorithm can be used to sort any kind of data type,
-// I used typedef to illustrate that it can be adapted to any other kind of data type.
-typedef int DATA_TYPE;
-
-void bubbleSort(DATA_TYPE *array, int arraySize)
+void bubbleSort(int* array, const int arraySize)
 {
-    DATA_TYPE temporaryValue = 0;
-    for(int i = 0; i < arraySize - 1 ; i++)
+    for(int i = 0; i < arraySize - 1; i++)
     {
         for(int j = 0; j < arraySize - 1; j++)
         {
             if(array[j+1] < array[j])
             {
-                temporaryValue = array[j];
-                array[j] = array[j+1];
-                array[j+1] = temporaryValue;
+                swap(array, j, j + 1);
             }
         }
     }
---
+++ SortingAlgorithms/bubblesort.h
@@ -0,0 +1,6 @@
+#ifndef __BUBBLE_SORT_H__
+#define __BUBBLE_SORT_H__
+
+void bubbleSort(int* array, const int arraySize);
+
+#endif
