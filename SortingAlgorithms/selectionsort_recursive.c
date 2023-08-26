d8181fd19d4a28e11342ca4c68112555b34a7639
Lucas <lucas.fn4@gmail.com>
2023-08-25 22:37:08 -0300

    Adds headers for recursive selectionsort

--- SortingAlgorithms/selectionsort_recursive.c
+++ SortingAlgorithms/selectionsort_recursive.c
@@ -1,21 +1,17 @@
-// This implementation of selectionSort is for integer arrays. But since the algorithm can be used to sort any kind of data type,
-// I used typedef to illustrate that it can be adapted to any other kind of data type.
-typedef int DATA_TYPE;
+#include "swap.h"
+#include "selectionsort_recursive.h"
 
-void selectionSort(DATA_TYPE *array, int leftIndex, int rightIndex)
+void selectionSort(int* array, int leftIndex, int rightIndex)
 {
     if(leftIndex == rightIndex) return;
 
     int smallestValuePosition = leftIndex;
-    for(int j = leftIndex+1 ; j<=rightIndex; j++)
+    for(int j = leftIndex + 1; j <= rightIndex; j++)
     {
         if(array[j] < array[smallestValuePosition])
             smallestValuePosition = j;
     }
 
-    DATA_TYPE temporaryValue = array[smallestValuePosition];
-    array[smallestValuePosition] = array[leftIndex];
-    array[leftIndex] = temporaryValue;
-
+    swap(array, smallestValuePosition, leftIndex);
     selectionSort(array,leftIndex+1,rightIndex);
 }
---
+++ SortingAlgorithms/selectionsort_recursive.h
@@ -0,0 +1,8 @@
+#ifndef __SELECTIONSORT_RECURSIVE_H__
+#define __SELECTIONSORT_RECURSIVE_H__
+
+#include "swap.h"
+
+void selectionSort(int* array, int leftIndex, int rightIndex);
+
+#endif
