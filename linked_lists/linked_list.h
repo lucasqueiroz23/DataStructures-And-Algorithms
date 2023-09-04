d3dab0e9a68204c2a3b6d6b83af6eac9862871f8
Lucas <lucas.fn4@gmail.com>
2023-09-04 19:11:13 -0300

    Adds header file for doubly linked list

---
+++ linked_lists/linked_list.h
@@ -0,0 +1,27 @@
+#ifndef __LINKED_LIST_H__
+#define __LINKED_LIST_H__
+
+typedef struct node {
+    int data;
+    struct node* next;
+    struct node* prev;
+} node;
+
+typedef struct linked_list {
+    int length;
+    node* head;
+    node* tail;
+} linked_list;
+
+linked_list* create_linked_list();
+
+void push(linked_list* ll, int data);
+
+int pop(linked_list* ll);
+
+int search(linked_list* ll, int target);
+
+int remove(linked_list* ll, int target);
+
+int update_value(linked_list* ll, int target, int new_value);
+#endif
